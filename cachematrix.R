5a6f830a814a15dad0f7b8b296a8027bf2ac4d00
Andrey <efimov.andevg@gmail.com>
2020-05-06 20:50:54 +0300

    Hello

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,48 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+  cacheMatrix <- NULL
+
+  setMatrix <- function(y) {
+    x <<- y
+    cacheMatrix <<- NULL
+  }
+
+  getMatrix <- function() x
+  setCache <- function(inverse) cacheMatrix <<- inverse
+  getCache <- function() cacheMatrix
+
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
+
+
+
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+
+  cacheMatrix <- x$getCache()
+
+  #if the content is not null, return the result
+
+  if (!is.null(cacheMatrix)) {
+    return(cacheMatrix)
+  }
+
+  # if the content is empty,
+  # get the matrix, create, set, update and return the cache matrix
+  else {
+    newMatrix <- x$getMatrix()
+    cacheMatrix <- solve(newMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  }
+
 }
